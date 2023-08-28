b978b831344b1e2132eff0419c7f0f494bdf6413
Reuben27 <reuben.sd@iitgn.ac.in>
2023-08-28 19:38:46 +0530

    Edit Codes

---
+++ Day 5/example.js
@@ -0,0 +1,37 @@
+ //Examples for different types of variables and data types are
+ const a = 1;
+ var name = "user";
+ //Data types
+ p = null; //Null data type
+ b = 5; //Number
+ message = "Hello world!"; //String
+ flag = true; //Boolean
+
+ //Array example
+ arr = [1, 2, 3, 4, 5, 6];
+ console.log(arr[0]); //print zeroth element of array
+
+ //object example
+ obj = { name: "user", roll: 53146 };
+
+ //Conditions
+ if (true) {
+   //if/else statement
+   console.log("Condition is true");
+ } else {
+   console.log("Condition is false");
+ }
+
+ //Loop
+ for (i = 0; i < 10; i++) {
+   //for loop
+   console.log(i);
+ }
+
+ function changecolor() {
+   //function example
+   close();
+ }
+
+ //event listener
+ document.getElementById("title").addEventListener("click", changecolor);
